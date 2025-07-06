4ce31bc350a68f6182bc26108da20647ed4aeefa
XxEr_Abinesh_MrxX <147540638+er-abinesh-21@users.noreply.github.com>
2025-07-06 18:08:22 +0530

    Update HeroSection.tsx

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -19,7 +19,7 @@ export const HeroSection = () => {
       <motion.div
         className="absolute inset-0 z-0"
         style={{
-          backgroundImage: `url(https://images.unsplash.com/photo-1558591710-4b4a1ae0f04d?q=80&w=1974&auto=format&fit=crop)`,
+          backgroundImage: `url(https://images.unsplash.com/photo-1579546929518-9e396f3cc809?q=80&w=1200&auto=format&fit=crop)`,
           backgroundPosition: "center",
           backgroundSize: "cover",
           y,
